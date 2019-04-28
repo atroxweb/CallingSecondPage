6108eac393315a3ca55d965f91dcd8c4135599af
Shah <atroxweb@gmail.com>
2019-04-28 04:23:35 -0400

    Initial commit

---
+++ app/src/main/java/ca/pufferfish/callingsecondpage/SeceondPage.java
@@ -0,0 +1,13 @@
+package ca.pufferfish.callingsecondpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeceondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seceond_page);
+    }
+}
